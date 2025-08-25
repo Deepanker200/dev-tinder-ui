2de997d56dc0744684f0d9fde49b8eb4e5d097c3
Deepanker Tiwari <deepamamazing@gmail.com>
2025-08-25 23:07:23 +0530

    Namaste Node- Season 2 DevTinder UI Part 2

--- README.md
+++ README.md
@@ -8,8 +8,11 @@
 - Create a Navbar.jsx seperate Component File
 - Install react router DOM
 - Create BrowserRouter/>
-
-
+- Create a Footer
+- Create a Login Page
+- Install axios
+- CORS- Install CORS in backend => add middleware to frontend with configurations: origin, credentials:truw
+- Whenever making API call so pass=>{withCredentials:true} for axios
 
 
 # Body
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+
+    const [emailId, setEmailId] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+        try {
+            const res = await axios.post("http://localhost:7777/login", {
+                emailId, password
+            },
+                { withCredentials: true }
+            )
+            console.log(res);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <div className='flex justify-center my-10'>
+            <div className="card card-border bg-base-300 w-96">
+                <div className="card-body">
+                    <h2 className="card-title justify-center">Login</h2>
+                    <label className="form-control w-full max-w-xs my-2">
+                        <div className="label my-1">
+                            <span className="label-text">Email ID</span>
+                        </div>
+                        <input type="text"
+                            value={emailId}
+                            onChange={(e) => { setEmailId(e.target.value) }}
+                            className="input input-bordered w-full max-w-xs" />
+                    </label>
+                    <label className="form-control w-full max-w-xs my-2">
+                        <div className="label my-1">
+                            <span className="label-text">Password</span>
+                        </div>
+                        <input type="text"
+                            value={password}
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            className="input input-bordered w-full max-w-xs" />
+                    </label>
+                    <div className="card-actions justify-center">
+                        <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login
