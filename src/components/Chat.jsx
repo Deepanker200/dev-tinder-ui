d2c4a624ea60d874e4737b6a25b646f5b2bf370a
Deepanker Tiwari <deepamamazing@gmail.com>
2025-09-16 21:45:19 +0530

    prod changes

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -85,7 +85,7 @@ const Chat = () => {
 };
 
     return (
-        <div className='w-72 md:w-3/4 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col'>
+        <div className='w-[350px] md:w-3/4 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col'>
             <h1 className='p-5 border-b border-gray-600'>Chat</h1>
             <div className='flex-1 overflow-scroll p-5'>
                 {messages.map((msg, index) => {
